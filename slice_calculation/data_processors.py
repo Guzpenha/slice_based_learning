ae035005c599c587ecca954aec4af7f7213262eb
Gustavo Penha <gustavopenha@wlan-145-94-214-241.wlan.tudelft.nl>
2019-10-16 11:09:42 +0200

    fork of transformers and initial slice functions with data processors

---
+++ slice_calculation/data_processors.py
@@ -0,0 +1,195 @@
+import os
+import csv
+import sys
+import copy
+import json
+
+from typing import List
+from tqdm import tqdm
+from IPython import embed
+
+csv.field_size_limit(sys.maxsize)
+
+def transform_to_q_docs_format(examples):
+    """
+    Groups examples of each query.
+    BE CAREFUL! This function assumes that every query has only one relevant document.
+    """
+    doc_q_examples = []
+    docs = []
+    labels = []
+    for i, example in enumerate(examples):
+        if example[2] == "1" and i!=0:
+            doc_q_examples.append([examples[i-1][0], docs, labels])
+            docs = []
+            labels = []
+        docs.append(example[1])
+        labels.append(example[2])
+
+        if i == len(examples):
+            doc_q_examples.append([example[0], docs, labels])
+    return doc_q_examples
+
+class InputExample(object):
+    """
+    A single training/test example for a retrieval task
+
+    Inputs
+    =========
+    q_id: unique identifier for the instance
+    query: a list containing the query texts, in conversational response ranking len(query)>=1
+    documents: a list containing the candidate documents to rank
+    label: a list with the relevance of the query wrt each document
+
+    """
+    def __init__(self, instance_id: str, query: List[str],
+                 documents: List[str], labels: List[str]=None):
+        self.instance_id = instance_id
+        self.query = query
+        self.documents = documents
+        self.labels = labels
+
+    def __repr__(self):
+        return str(self.to_json_string())
+
+    def to_dict(self):
+        """Serializes this instance to a Python dictionary."""
+        output = copy.deepcopy(self.__dict__)
+        return output
+
+    def to_json_string(self):
+        """Serializes this instance to a JSON string."""
+        return json.dumps(self.to_dict(), indent=2, sort_keys=True) + "\n"
+
+class DataProcessor(object):
+    """Base class for data converters for retrieval tasks"""
+
+    def get_train_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the train set."""
+        raise NotImplementedError()
+
+    def get_dev_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the dev set."""
+        raise NotImplementedError()
+
+    @classmethod
+    def _read_tsv(cls, input_file, quotechar=None):
+        """Reads a tab separated value file."""
+        with open(input_file, "r", encoding="utf-8-sig") as f:
+            reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
+            lines = []
+            for line in reader:
+                if sys.version_info[0] == 2:
+                    line = list(unicode(cell, 'utf-8') for cell in line)
+                lines.append(line)
+            return lines
+
+class CRRProcessor(DataProcessor):
+    """Processor for the Conversation Response Ranking datasets, such as MSDialog, UDC and MANtIS."""
+
+    def get_train_examples(self, data_dir):
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.txt")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "valid.txt")), "dev")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.txt")), "test")
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        q_doc_lines = []
+        for (i, line) in enumerate(lines):
+            query = line[1:-1]
+            doc = line[-1][0:-2]
+            label = line[0]
+            q_doc_lines.append([query, doc, label])
+        lines_by_q = transform_to_q_docs_format(q_doc_lines)
+        examples = []
+        for i, line in enumerate(lines_by_q):
+            id = "%s-%s" % (set_type, i)
+            examples.append(InputExample(instance_id=id, query=line[0], documents=line[1], labels=line[2]))
+        return examples
+
+class QuoraProcessor(DataProcessor):
+    """Processor for the QUORA data set."""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "quora_train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "quora_dev.tsv")), "dev")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "quora_test.tsv")), "test")
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        q_doc_lines = []
+        for (i, line) in enumerate(lines):
+            query = line[3]
+            doc = line[4]
+            if line[0] == "0":
+                label = "1"
+            else:
+                label = "0"
+            q_doc_lines.append([query, doc, label])
+        lines_by_q = transform_to_q_docs_format(q_doc_lines)
+        examples = []
+        for i, line in enumerate(lines_by_q):
+            id = "%s-%s" % (set_type, i)
+            examples.append(InputExample(instance_id=id, query=[line[0]], documents=line[1], labels=line[2]))
+        return examples
+
+class QAProcessor(DataProcessor):
+    """Processor for the L4 data set."""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        q_doc_lines = []
+        for (i, line) in enumerate(lines):
+            label = line[0]
+            query = line[1]
+            doc = line[2]
+            q_doc_lines.append([query, doc, label])
+        lines_by_q = transform_to_q_docs_format(q_doc_lines)
+        examples = []
+        for i, line in enumerate(lines_by_q):
+            id = "%s-%s" % (set_type, i)
+            examples.append(InputExample(instance_id=id, query=[line[0]], documents=line[1], labels=line[2]))
+        return examples
+
+processors = {
+    "quora": QuoraProcessor,
+    "mantis_10": CRRProcessor,
+    "ms_v2": CRRProcessor,
+    "udc": CRRProcessor,
+    "l4": QAProcessor,
+    "ms_marco_adhoc": QAProcessor
+}
---
+++ transformers/transformers/data/metrics/__init__.py
@@ -0,0 +1,203 @@
+# coding=utf-8
+# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+import logging
+
+logger = logging.getLogger(__name__)
+
+try:
+    from scipy.stats import pearsonr, spearmanr
+    from sklearn.metrics import matthews_corrcoef, f1_score
+    import random
+    import numpy as np
+    _has_sklearn = True
+except (AttributeError, ImportError) as e:
+    logger.warning("To use data.metrics please install scikit-learn. See https://scikit-learn.org/stable/index.html")
+    _has_sklearn = False
+
+def is_sklearn_available():
+    return _has_sklearn
+
+if _has_sklearn:
+
+    def simple_accuracy(preds, labels):
+        return (preds == labels).mean()
+
+
+    def acc_and_f1(preds, labels):
+        acc = simple_accuracy(preds, labels)
+        f1 = f1_score(y_true=labels, y_pred=preds)
+        return {
+            "acc": acc,
+            "f1": f1,
+            "acc_and_f1": (acc + f1) / 2,
+        }
+
+
+    def _to_list(x):
+        if isinstance(x, list):
+            return x
+        return [x]
+
+    def ap(y_true, y_pred, rel_threshold=0):
+        s = 0.
+        y_true = _to_list(np.squeeze(y_true).tolist())
+        y_pred = _to_list(np.squeeze(y_pred).tolist())
+        c = [a for a in zip(y_true, y_pred)]
+        random.shuffle(c)
+        c = sorted(c, key=lambda x: x[1], reverse=True)
+        ipos = 0
+        for j, (g, p) in enumerate(c):
+            if g > rel_threshold:
+                ipos += 1.
+                s += ipos / (j + 1.)
+        if ipos == 0:
+            s = 0.
+        else:
+            s /= ipos
+        return s
+
+
+    def ndcg(k=10):
+        def top_k(y_true, y_pred, rel_threshold=0.):
+            if k <= 0.:
+                return 0.
+            s = 0.
+            y_true = _to_list(np.squeeze(y_true).tolist())
+            y_pred = _to_list(np.squeeze(y_pred).tolist())
+            c = [v for v in zip(y_true, y_pred)]
+            random.shuffle(c)
+            c_g = sorted(c, key=lambda x: x[0], reverse=True)
+            c_p = sorted(c, key=lambda x: x[1], reverse=True)
+            idcg = 0.
+            ndcg = 0.
+            for i, (g, p) in enumerate(c_g):
+                if i >= k:
+                    break
+                if g > rel_threshold:
+                    idcg += (math.pow(2., g) - 1.) / math.log(2. + i)
+            for i, (g, p) in enumerate(c_p):
+                if i >= k:
+                    break
+                if g > rel_threshold:
+                    ndcg += (math.pow(2., g) - 1.) / math.log(2. + i)
+            if idcg == 0.:
+                return 0.
+            else:
+                return ndcg / idcg
+
+        return top_k
+
+
+    def ndcg_at_10(preds, labels):
+        ndcgs = []
+        query_preds = []
+        query_labels = []
+        i = 0
+        ndcg_at_10 = ndcg(k=10)
+        for l, p in zip(labels, preds):
+            if (l == 1 and i != 0):
+                ndcgs.append(ndcg_at_10(query_labels, query_preds))
+                query_preds = []
+                query_labels = []
+
+            query_preds.append(p)
+            query_labels.append(l)
+
+            i += 1
+
+            if (i == len(preds)):
+                ndcgs.append(ndcg_at_10(query_labels, query_preds))
+        return sum(ndcgs) / float(len(ndcgs))
+
+    def mean_average_precision(preds, labels):
+        aps = []
+        query_preds = []
+        query_labels = []
+        i = 0
+        for l, p in zip(labels, preds):
+            if (l == 1 and i != 0):
+                aps.append(ap(query_labels, query_preds))
+                query_preds = []
+                query_labels = []
+
+            query_preds.append(p)
+            query_labels.append(l)
+
+            i += 1
+
+            if (i == len(preds)):
+                aps.append(ap(query_labels, query_preds))
+        return sum(aps) / float(len(aps))
+
+
+    def compute_aps(preds, labels):
+        aps = []
+        query_preds = []
+        query_labels = []
+        i = 0
+        for l, p in zip(labels, preds):
+            if (l == 1 and i != 0):
+                aps.append(ap(query_labels, query_preds))
+                query_preds = []
+                query_labels = []
+
+            query_preds.append(p)
+            query_labels.append(l)
+
+            i += 1
+
+            if (i == len(preds)):
+                aps.append(ap(query_labels, query_preds))
+        return aps
+
+    def pearson_and_spearman(preds, labels):
+        pearson_corr = pearsonr(preds, labels)[0]
+        spearman_corr = spearmanr(preds, labels)[0]
+        return {
+            "pearson": pearson_corr,
+            "spearmanr": spearman_corr,
+            "corr": (pearson_corr + spearman_corr) / 2,
+        }
+
+
+    def glue_compute_metrics(task_name, preds, labels):
+        assert len(preds) == len(labels)
+        if task_name in ["quora", "mantis_10", "ms_v2", "udc"]:
+            return {"map": mean_average_precision(preds, labels), "ndcg_10": ndcg_at_10(preds, labels)}
+        if task_name == "cola":
+            return {"mcc": matthews_corrcoef(labels, preds)}
+        elif task_name == "sst-2":
+            return {"acc": simple_accuracy(preds, labels)}
+        elif task_name == "mrpc":
+            return acc_and_f1(preds, labels)
+        elif task_name == "sts-b":
+            return pearson_and_spearman(preds, labels)
+        elif task_name == "qqp":
+            return acc_and_f1(preds, labels)
+        elif task_name == "mnli":
+            return {"acc": simple_accuracy(preds, labels)}
+        elif task_name == "mnli-mm":
+            return {"acc": simple_accuracy(preds, labels)}
+        elif task_name == "qnli":
+            return {"acc": simple_accuracy(preds, labels)}
+        elif task_name == "rte":
+            return {"acc": simple_accuracy(preds, labels)}
+        elif task_name == "wnli":
+            return {"acc": simple_accuracy(preds, labels)}
+        else:
+            raise KeyError(task_name)
